9b6192c06f6841ce396926dbd7d2a960b043d67c
Michael Woolnough <michael.woolnough@gmail.com>
2016-03-18 16:19:53 +0000

    Changed triangle renderer to match new coords system

--- triangle_gl21.go
+++ triangle_gl21.go
@@ -5,14 +5,17 @@ import "github.com/go-gl/gl/v2.1/gl"
 func (t triangle) Render(time float64) {
 
 	gl.LoadIdentity()
+	gl.PushMatrix()
+	gl.Translatef(0.5, 0.5, 0)
 	gl.Rotated(time*50, 0, 0, 1)
 
 	gl.Begin(gl.TRIANGLES)
 	gl.Color3f(1, 0, 0)
-	gl.Vertex3f(-0.6, -0.4, 0.)
+	gl.Vertex3f(-0.4, -0.231, 0.)
 	gl.Color3f(0, 1, 0)
-	gl.Vertex3f(0.6, -0.4, 0)
+	gl.Vertex3f(0.4, -0.231, 0)
 	gl.Color3f(0, 0, 1)
-	gl.Vertex3f(0, 0.6, 0)
+	gl.Vertex3f(0, 0.46182, 0)
 	gl.End()
+	gl.PopMatrix()
 }
