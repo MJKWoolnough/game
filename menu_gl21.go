88c5e495e935e449b5ea3fc974b9af4b8555b195
Michael Woolnough <michael.woolnough@gmail.com>
2016-03-20 16:55:38 +0000

    Added pseudo-blocks to make code easier to read

--- menu_gl21.go
+++ menu_gl21.go
@@ -27,18 +27,22 @@ func (m *Menu) Render(time float64) {
 		top := m.top + float64(num)*m.gap
 		left := m.left
 		gl.Begin(gl.TRIANGLE_STRIP)
-		gl.Color3f(1, 0, 0)
-		gl.Vertex3d(left+m.width, top, 0)
-		gl.Vertex3d(left, top, 0)
-		gl.Vertex3d(left+m.width, top+m.height, 0)
-		gl.Vertex3d(left, top+m.height, 0)
+		{
+			gl.Color3f(1, 0, 0)
+			gl.Vertex3d(left+m.width, top, 0)
+			gl.Vertex3d(left, top, 0)
+			gl.Vertex3d(left+m.width, top+m.height, 0)
+			gl.Vertex3d(left, top+m.height, 0)
+		}
 		gl.End()
 		gl.PushMatrix()
-		var vp [4]int32
-		gl.GetIntegerv(gl.VIEWPORT, &vp[0])
-		gl.Translated(left*float64(vp[2]), -top*float64(vp[3]), 0)
-		gl.Color3f(0, 1, 0)
-		menuFont.Printf(0, 0, item.Name)
+		{
+			var vp [4]int32
+			gl.GetIntegerv(gl.VIEWPORT, &vp[0])
+			gl.Translated(left*float64(vp[2]), -top*float64(vp[3]), 0)
+			gl.Color3f(0, 1, 0)
+			menuFont.Printf(0, 0, item.Name)
+		}
 		gl.PopMatrix()
 	}
 }
